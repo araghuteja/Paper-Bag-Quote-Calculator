f7ee4a374b0ac3617969735130fe2f0d073b90a9
rauat <rauat@tuta.io>
2020-01-04 02:36:41 +0530

    Added all formulae

--- src/app/calculator-form/calculator-form.component.ts
+++ src/app/calculator-form/calculator-form.component.ts
@@ -19,16 +19,50 @@ export class CalculatorFormComponent implements OnInit {
     margin: new FormControl('')
   });
 
-  constructor() { }
+  Math: any;
+  Constructor() {
+      this.Math = Math;
+  }
 
   ngOnInit() {
+    this.Math = Math;
   }
 
   getSheetSize() {
-    return ( this.bagDimensionsForm.value.height 
-              + (3 / 4 * this.bagDimensionsForm.value.bottom)
-              + this.bagDimensionsForm.value.lip )
-            * (2 * this.bagDimensionsForm.value.width + 2 * this.bagDimensionsForm.value.bottom) ;
+    return this.getSheetLength() + ' x ' + this.getSheetWidth();
   }
 
+  getSheetLength(): number {
+    return ( this.bagDimensionsForm.value.height 
+      + (3 / 4 * this.bagDimensionsForm.value.bottom)
+      + this.bagDimensionsForm.value.lip );
+  }
+
+  getSheetWidth(): number {
+    return (2 * this.bagDimensionsForm.value.width + 2 * this.bagDimensionsForm.value.bottom);
+  }
+
+  getWeightPerSheet(): number{
+    return Number((this.getSheetLength() * this.getSheetWidth() / 10000 * this.bagDimensionsForm.value.paperThickness).toFixed(2));
+  }
+
+  getNumberOfSheetPerKg(): number {
+    return this.getWeightPerSheet() ? Number((1000 / this.getWeightPerSheet()).toFixed(2)) : 0;
+  }
+
+  getCostPerUnit(): number {
+    return this.getNumberOfSheetPerKg() ? Number((this.bagDimensionsForm.value.ratePerKilo / Math.floor(this.getNumberOfSheetPerKg())).toFixed(2)) : 0;
+  }
+
+  getCostToPrint(): number {
+    return this.getNumberOfSheetPerKg() ? Number((this.bagDimensionsForm.value.costToPrint * Math.floor(this.getNumberOfSheetPerKg())).toFixed(2)) : 0;
+  }
+
+  getRatePerUnit(): number {
+    return  this.getCostPerUnit() || this.getCostToPrint() ? Number((1.8 * (this.getCostPerUnit() + this.getCostToPrint() + 3.19)).toFixed(2)) : 0;
+  }
+
+  getSheetSellingPrice() {
+    return this.getCostPerUnit() || this.getCostToPrint() ? ((1 + (this.bagDimensionsForm.value.margin / 100)) * this.getRatePerUnit()).toFixed(2) : 0;
+  }
 }
